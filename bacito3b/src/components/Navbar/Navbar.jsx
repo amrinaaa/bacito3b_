713339c524bcb27710a73c59c22476dfd220fec5
amrina rossada <amrina0612@gmail.com>
2024-08-30 16:39:20 +0800

    update

--- bacito3b/src/components/Navbar/Navbar.jsx
+++ bacito3b/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="fixed top-0 right-0 w-full bg-white text-black shadow-md">
+      <div>
+      <div className="container bg-gradient-to-r from-primary to secondary">
+        <div className="flex justify-between py-[2px]">
+          <p>20% off on next booking</p>
+          <p>Mobile No. +91 123456789</p>
+      </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
